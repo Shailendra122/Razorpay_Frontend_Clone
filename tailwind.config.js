c0c5552cc1a57e49ff63f1e4b60509f78c0e15d0
Shailendra <shailendrashrivastav1292@gmail.com>
2024-07-11 23:14:55 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./*.html"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        deepBlue: '#060E26', // Replace with your desired color code
+      },
+    },
+  },
+  plugins: [],
+}
